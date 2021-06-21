9ee10ed22c58277e40b0f3ecd70eb83c8e4bdbbd
Elena <chitoroglena@icloud.com>
2021-06-21 23:43:55 +0300

    Added html and js.

---
+++ app.js
@@ -0,0 +1,6 @@
+const kelvin = 293; // The value saved to kelvin will stay constant
+
+const celsius = kelvin - 273; // Converting Kelvin to Celsius
+
+const fahrenheit = Math.floor(celsius * (9/5) + 32); ////Converts Celsius to Fahrenheit and rounds it down
+console.log(`The temperature is ${fahrenheit} degress Fahrenheit`);
