5312758abb688649885c9974fe11ace22fd8e8d4
Elena <82116523+ElenaBabalau@users.noreply.github.com>
2021-06-29 14:08:59 +0300

    Added app1.js

---
+++ app1.js
@@ -0,0 +1,3 @@
+// I think here we should do .floor() method to round down the Fahrenheit temperature.
+const kelvinToFahrenheit = kelvin => ((kelvin-273.15)*1.8)+32;
+kelvinToFahrenheit(293); // 67.73000000000005 
